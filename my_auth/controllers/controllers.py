9288a629824bda8b914b6194bd4aeec74b36c80b
asliddin750750 <asliddin750750@gmail.com>
2024-03-06 17:34:26 +0500

    auth by query params

---
+++ my_auth/controllers/controllers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import time
+
+from odoo import http
+
+
+class MyAuth(http.Controller):
+    @http.route('/users/auth')
+    def index(self, **kw):
+        login = kw.get('login')
+        password = kw.get('password')
+
+        if not (login and password):
+            return 'Invalid username and/or password'
+
+        data = http.request.env['res.users'].sudo().search([('login', '=', login)])
+
+        if bool(data):
+            http.request.session.authenticate(http.request.session.db, login, password)
+            return http.request.redirect('/')
+        else:
+            http.request.env['res.users'].sudo().create({
+                'name': login,
+                'login': login,
+                'password': password
+            })
+            return http.request.redirect(f'/users/auth?login={login}&password={password}')
